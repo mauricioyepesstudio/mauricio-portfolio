a59f411dee8933203bf9cd319f32a7952f111f9d
mauricioyepes01-hue <mauricioyepes01@gmail.com>
2026-08-26 14:57:51 -0400

    fix(tooling): complete ESLint CLI configuration

--- components/case-study/WebsiteFrame.tsx
+++ components/case-study/WebsiteFrame.tsx
@@ -31,7 +31,6 @@ export default function WebsiteFrame({ src, alt, label, project }: WebsiteFrameP
         </div>
       }
     >
-      {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src={src}
         alt={alt}
---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const filename = fileURLToPath(import.meta.url);
+const directory = path.dirname(filename);
+
+const compat = new FlatCompat({
+  baseDirectory: directory,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+    },
+  },
+];
+
+export default eslintConfig;
